61ae812a90ece6fca4bdd40310efc4b5f80ac4f8
Shaan Malik <99844393+Shaan6695@users.noreply.github.com>
2024-10-18 00:44:15 -0400

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,1 +1,12 @@
+//Playwright Tests for Agile Docs
 
+
+test('Verify homepage link, title, and article heading', async ({ page }) => {
+  // Go to the homepage
+  await page.goto('https://dfianuale.github.io/hexo-IS373/');
+
+  // Check the homepage URL
+  await expect(page).toHaveURL('https://dfianuale.github.io/hexo-IS373/');
+
+  // Verify the page title
+  await expect(page).toHaveTitle(/Hexo/); // Assuming that "Hexo" is part of title
